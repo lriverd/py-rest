5f43b52769a4ec37c986a8a6dac6489e93eacbb0
Luis Riveros <luis66riv@gmail.com>
2018-02-27 13:58:23 -0300

    C2

--- main.py
+++ main.py
@@ -3,7 +3,12 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-  return 'Hey its Python Flask application!'
+  return 'Servidor Up'
+
+@app.route('/status', methods=['GET'])
+def get_status():
+	output = {'Status': 'Ok'}
+	return jsonify({'result': output})
 
 if __name__ == '__main__':
   app.run()
